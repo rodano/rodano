a2f62e0d1ccde03216142a783bf7d292e559c6d6
Matthieu Corageoud <matthieu.corageoud@usb.ch>
2025-07-30 16:46:06 +0200

    Fix the Karma configuration used for the CI

--- frontends/main/src/karma-ci.conf.cjs
+++ frontends/main/src/karma-ci.conf.cjs
@@ -5,8 +5,7 @@ module.exports = function(config) {
 		plugins: [
 			require('karma-jasmine'),
 			require('karma-chrome-launcher'),
-			require('karma-htmlfile-reporter'),
-			require('@angular-devkit/build-angular/plugins/karma')
+			require('karma-htmlfile-reporter')
 		],
 		client: {
 			clearContext: false // leave Jasmine Spec Runner output visible in browser
