79f008a3b97749b0374dbc2c2ddf8389c7839fb7
Matthieu Corageoud <matthieu.corageoud@usb.ch>
2025-10-31 18:00:02 +0100

    Fix display of the network error page

--- frontends/main/src/app/app.component.ts
+++ frontends/main/src/app/app.component.ts
@@ -17,23 +17,30 @@ export class AppComponent implements OnInit {
 	environment = Environment;
 
 	study?: PublicStudy;
+	displayContent = false;
 
 	ngOnInit() {
 		//set the CSS color variables to the body
 		this.configurationService.getPublicStudy()
-			.subscribe(study => {
-				this.study = study;
+			.subscribe({
+				next: study => {
+					document.body.style.setProperty('--mat-sys-primary', study.color);
+					document.body.style.setProperty('--mat-sys-on-primary', 'white');
 
-				document.body.style.setProperty('--mat-sys-primary', study.color);
-				document.body.style.setProperty('--mat-sys-on-primary', 'white');
+					document.body.style.setProperty('--mat-sys-outline', study.color);
+					//document.body.style.setProperty('--mat-icon-color', study.color);
 
-				document.body.style.setProperty('--mat-sys-outline', study.color);
-				//document.body.style.setProperty('--mat-icon-color', study.color);
+					document.body.style.setProperty('--mat-sys-primary-container', 'color(from var(--mat-sys-primary) display-p3 calc(r - 0.1) calc(g - 0.1) calc(b - 0.1))');
+					document.body.style.setProperty('--mat-sys-on-primary-container', 'white');
+					document.body.style.setProperty('--mat-sys-secondary-container', 'color(from var(--mat-sys-primary) display-p3 calc(r - 0.1) calc(g - 0.1) calc(b - 0.1))');
+					document.body.style.setProperty('--mat-sys-on-secondary-container', 'white');
 
-				document.body.style.setProperty('--mat-sys-primary-container', 'color(from var(--mat-sys-primary) display-p3 calc(r - 0.1) calc(g - 0.1) calc(b - 0.1))');
-				document.body.style.setProperty('--mat-sys-on-primary-container', 'white');
-				document.body.style.setProperty('--mat-sys-secondary-container', 'color(from var(--mat-sys-primary) display-p3 calc(r - 0.1) calc(g - 0.1) calc(b - 0.1))');
-				document.body.style.setProperty('--mat-sys-on-secondary-container', 'white');
+					this.study = study;
+					this.displayContent = true;
+				},
+				error: () => {
+					this.displayContent = true;
+				}
 			});
 	}
 }
