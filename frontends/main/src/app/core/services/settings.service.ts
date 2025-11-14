d6e33686a438e9c793969fa55b77676ae9707e7c
Matthieu Corageoud <matthieu.corageoud@usb.ch>
2025-11-14 17:27:11 +0100

    Add missing settings service

---
+++ frontends/main/src/app/core/services/settings.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class SettingsService {
+	private static STORAGE_KEY = 'settings';
+
+	settings: Record<string, any> = {};
+
+	constructor() {
+		this.settings = JSON.parse(localStorage.getItem(SettingsService.STORAGE_KEY) || '{}');
+	}
+
+	saveSettings() {
+		localStorage.setItem(SettingsService.STORAGE_KEY, JSON.stringify(this.settings));
+	}
+
+	set(key: string, value: any) {
+		this.settings[key] = value;
+		this.saveSettings();
+	}
+
+	get<T>(key: string, defaultValue: T): T {
+		if(this.settings.hasOwnProperty(key)) {
+			return this.settings[key];
+		}
+		return defaultValue;
+	}
+}
