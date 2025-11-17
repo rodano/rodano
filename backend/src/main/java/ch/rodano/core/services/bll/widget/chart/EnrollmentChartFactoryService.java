1edad579082da2d38b28c7826bd8f13de1d97ad4
farisahmetasevic <faris.ahmetasevic@hotmail.com>
2025-11-17 15:44:54 +0100

    QA Remove unnecessary types

--- backend/src/main/java/ch/rodano/core/services/bll/widget/chart/EnrollmentChartFactoryService.java
+++ backend/src/main/java/ch/rodano/core/services/bll/widget/chart/EnrollmentChartFactoryService.java
@@ -73,20 +73,20 @@ public class EnrollmentChartFactoryService {
 			final var downsampledResults = ChartHelpers.downsample(cumulatedResults, ChartHelpers.DEFAULT_MAX_POINTS);
 			final var timepoints = ChartHelpers.processTimepoints(downsampledResults);
 
-			datasets.add(new ChartDatasetDTO<ZonedDateTime, Integer>(scope.getCode(), timepoints));
+			datasets.add(new ChartDatasetDTO<>(scope.getCode(), timepoints));
 
 			//expected data
 			final var expectedTimepoints = new ArrayList<ChartDatasetPoint<ZonedDateTime, Integer>>();
 			if(chart.isDisplayExpected() && !scope.getData().getEnrollmentTargets().isEmpty()) {
-				expectedTimepoints.add(new ChartDatasetPoint<ZonedDateTime, Integer>(scope.getData().getEnrollmentStart(), 0));
+				expectedTimepoints.add(new ChartDatasetPoint<>(scope.getData().getEnrollmentStart(), 0));
 
 				//middle points
 				scope.getData().getEnrollmentTargets()
 					.stream()
-					.map(target -> new ChartDatasetPoint<ZonedDateTime, Integer>(target.getDate(), target.getExpectedNumber()))
+					.map(target -> new ChartDatasetPoint<>(target.getDate(), target.getExpectedNumber()))
 					.forEach(expectedTimepoints::add);
 
-				datasets.add(new ChartDatasetDTO<ZonedDateTime, Integer>(String.format("%s (expected)", scope.getCode()), expectedTimepoints));
+				datasets.add(new ChartDatasetDTO<>(String.format("%s (expected)", scope.getCode()), expectedTimepoints));
 			}
 		}
 		return datasets;
