c0dc9a9e642b4692b8bb2666df64915622b7dde3
Matthieu Corageoud <matthieu.corageoud@usb.ch>
2026-09-23 18:25:58 +0200

    Simplify frontend bootstrap

--- frontends/main/src/app/app.component.ts
+++ frontends/main/src/app/app.component.ts
@@ -22,7 +22,6 @@ export class AppComponent implements OnInit {
 
 	readonly study = signal<PublicStudy | undefined>(undefined);
 	readonly inMaintenance = signal(false);
-	readonly displayContent = signal(false);
 
 	ngOnInit() {
 		this.administrationService.isInMaintenance().subscribe(inMaintenance => {
@@ -40,28 +39,21 @@ export class AppComponent implements OnInit {
 
 	private loadStudy() {
 		//set the CSS color variables to the body
-		this.configurationService.getPublicStudy()
-			.subscribe({
-				next: study => {
-					document.body.style.setProperty('--mat-sys-primary', study.color);
-					document.body.style.setProperty('--mat-sys-on-primary', 'white');
+		this.configurationService.getPublicStudy().subscribe(study => {
+			document.body.style.setProperty('--mat-sys-primary', study.color);
+			document.body.style.setProperty('--mat-sys-on-primary', 'white');
 
-					document.body.style.setProperty('--mat-sys-outline', study.color);
-					//document.body.style.setProperty('--mat-icon-color', study.color);
+			document.body.style.setProperty('--mat-sys-outline', study.color);
+			//document.body.style.setProperty('--mat-icon-color', study.color);
 
-					document.body.style.setProperty('--mat-sys-primary-container', 'color(from var(--mat-sys-primary) display-p3 calc(r - 0.1) calc(g - 0.1) calc(b - 0.1))');
-					document.body.style.setProperty('--mat-sys-on-primary-container', 'white');
-					document.body.style.setProperty('--mat-sys-secondary-container', 'color(from var(--mat-sys-primary) display-p3 calc(r - 0.1) calc(g - 0.1) calc(b - 0.1))');
-					document.body.style.setProperty('--mat-sys-on-secondary-container', 'white');
+			document.body.style.setProperty('--mat-sys-primary-container', 'color(from var(--mat-sys-primary) display-p3 calc(r - 0.1) calc(g - 0.1) calc(b - 0.1))');
+			document.body.style.setProperty('--mat-sys-on-primary-container', 'white');
+			document.body.style.setProperty('--mat-sys-secondary-container', 'color(from var(--mat-sys-primary) display-p3 calc(r - 0.1) calc(g - 0.1) calc(b - 0.1))');
+			document.body.style.setProperty('--mat-sys-on-secondary-container', 'white');
 
-					document.body.style.setProperty('--themed-highlight-color', 'color-mix(in srgb, var(--mat-sys-primary) 15%, white)');
+			document.body.style.setProperty('--themed-highlight-color', 'color-mix(in srgb, var(--mat-sys-primary) 15%, white)');
 
-					this.study.set(study);
-					this.displayContent.set(true);
-				},
-				error: () => {
-					this.displayContent.set(true);
-				}
-			});
+			this.study.set(study);
+		});
 	}
 }
